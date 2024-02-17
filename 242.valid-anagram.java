09fdcefe1f843bef0451646fb787f374ed01c49c
Khoa Dang <khoahc98@gmail.com>
2024-02-17 12:31:51 +0700

    242

---
+++ 242.valid-anagram.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+//Given two strings s and t, return true if t is an anagram of s, and false 
+//otherwise. 
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a 
+//different word or phrase, typically using all the original letters exactly once. 
+//
+// 
+// Example 1: 
+// Input: s = "anagram", t = "nagaram"
+//Output: true
+// 
+// Example 2: 
+// Input: s = "rat", t = "car"
+//Output: false
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s and t consist of lowercase English letters. 
+// 
+//
+// 
+// Follow up: What if the inputs contain Unicode characters? How would you 
+//adapt your solution to such a case? 
+//
+// Related Topics Hash Table String Sorting 👍 11694 👎 376
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+// @lc code=end
+
