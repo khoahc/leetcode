1f376b80be3376a9844a264352d5d81f1d53003b
Khoa Dang <khoahc98@gmail.com>
2024-03-02 15:17:25 +0700

    66

---
+++ 66.plus-one.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;            
+        }        
+
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
+// @lc code=end
+
